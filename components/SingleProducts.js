fa513d737d45de09d194239ced5906466fdb0227
franapel <perezlefiman2@gmail.com>
2021-09-30 17:23:41 -0300

    Animar todo el componente de detalle del producto

--- components/SingleProducts.js
+++ components/SingleProducts.js
@@ -5,9 +5,9 @@ export default function SingleProduct({ business, product }) {
     const myLoader = ({ src, width, quality }) => `${src}?w=${width}&q=${quality || 75}`
 
     return (
-        <div className="flex flex-col p-3 border">
-            
-            <Image className="animate-fade" loader={myLoader} src={imgUrl} width={400} height={400} objectFit={'contain'} />
+        <div className="animate-fade flex flex-col p-3 border">
+
+            <Image loader={myLoader} src={imgUrl} width={400} height={400} objectFit={'contain'} />
 
             <p className="text-2xl">{product.name}</p>
             <p className="text-md">{product.name}</p>
