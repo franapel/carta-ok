a2da36710363c87f91191349fc3d0d49f20411ea
franapel <perezlefiman2@gmail.com>
2021-10-04 13:46:58 -0300

    Hacer options responsive

--- components/Options.js
+++ components/Options.js
@@ -1,22 +1,22 @@
 export default function Options({ options }) {
     return (
-        <div className="w-1/3">
+        <div className="sm:w-full md:w-1/2 lg:w-1/3">
 
             <form>
                 {options.map(option => {
                     return (
                         <div key={option.name} className="min-w-full">
                             <h3 className="flex h-16 bg-gray-100 px-3 items-center font-bold">{option.name}</h3>
-                            {option.opts.map((opt, i, {length}) => {
+                            {option.opts.map((opt, i, { length }) => {
                                 return (
-                                    <>
-                                    <div className="flex justify-between items-center h-16 px-3" key={opt}>
-                                        <label htmlFor={opt}>{opt}</label>
-                                        <input type="checkbox"  id={opt} name={opt}
-                                        className="h-5 w-5 rounded-sm "/>
+                                    <div key={opt}>
+                                        <div className="flex justify-between items-center h-16 px-3">
+                                            <label htmlFor={opt}>{opt}</label>
+                                            <input type="checkbox" id={opt} name={opt}
+                                                className="h-5 w-5 rounded-sm " />
+                                        </div>
+                                        {i + 1 != length && <hr className="mx-2" />}
                                     </div>
-                                    {i+1 != length && <hr className="mx-2"/>}
-                                    </>
                                 )
                             })}
                         </div>
