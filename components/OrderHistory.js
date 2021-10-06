84290d5c0599b41b52750202cd36cfeb56d97a36
franapel <perezlefiman2@gmail.com>
2021-10-06 12:31:16 -0300

    Agregar highlight al pedido al presionar el boton Detalle

--- components/OrderHistory.js
+++ components/OrderHistory.js
@@ -1,8 +1,15 @@
 export default function OrderHistory({ orders, products }) {
 
-    const handleDetailsClick = (id, e) => {
-        if (document.getElementById(id).classList.toggle("hidden")) e.target.innerText = "Detalle"
-        else e.target.innerText = "Ocultar detalle"
+    const handleDetailsClick = (higlightId, detailId, e) => {
+        if (document.getElementById(detailId).classList.toggle("hidden")) {
+            e.target.innerText = "Detalle"
+            document.getElementById(higlightId).classList.remove("bg-green-100")
+        }
+        else {
+            e.target.innerText = "Ocultar detalle"
+            document.getElementById(higlightId).classList.add("bg-green-100")
+        }
+        
     }
 
 
@@ -12,27 +19,30 @@ export default function OrderHistory({ orders, products }) {
             {orders.map(order => {
                 return (
                     <div key={order.id} className="mb-1">
-                        <div className="flex px-3 py-2 border rounded-sm">
-                            <div className="w-3/5">
-                                <h3 className="mb-1 text-xs lg:text-base">Pedido:</h3>
-                                {order.order_products.map(op =>
-                                    <p key={op.product_id} className="text-sm lg:text-xl">
-                                        {`${op.quantity}x ${products.find(p => p.id === op.product_id).name}`}
-                                    </p>)
-                                }
-                            </div>
-                            <div className="w-2/5 pt-8 flex flex-col justify-end">
-                                <button className="w-full mb-3 py-1 bg-green-600 rounded-sm text-xs lg:text-base text-white">Agregar al carro</button>
-                                <button onClick={(e) => handleDetailsClick(order.id, e)} className="w-full py-1 bg-gray-600 rounded-sm text-xs lg:text-base text-white">Detalle</button>
+                        <div className="p-1" id={order.id + "highlight"}>
+                            <div className="flex px-3 py-2 border rounded-sm">
+                                <div className="w-3/5">
+                                    <h3 className="mb-1 text-xs lg:text-base">Pedido:</h3>
+                                    {order.order_products.map(op =>
+                                        <p key={op.product_id} className="text-sm lg:text-xl">
+                                            {`${op.quantity}x ${products.find(p => p.id === op.product_id).name}`}
+                                        </p>)
+                                    }
+                                </div>
+                                <div className="w-2/5 pt-8 flex flex-col justify-end">
+                                    <button className="w-full mb-3 py-1 bg-green-600 rounded-sm text-xs lg:text-base text-white">Agregar al carro</button>
+                                    <button onClick={(e) => handleDetailsClick(order.id + "highlight", order.id + "detail", e)} className="w-full py-1 bg-gray-600 rounded-sm text-xs lg:text-base text-white">Detalle</button>
+                                </div>
                             </div>
                         </div>
 
 
-                        <div className="px-4 py-3 border border-t-0 bg-gray-200 hidden animate-grow origin-top" id={order.id}>
+                        <div className="mx-1 px-4 py-3 border border-t-0 bg-gray-200 hidden animate-grow origin-top" id={order.id + "detail"}>
                             <p className="text-xs lg:text-base">Precio total: {order.total_price}</p>
                             <p className="text-xs lg:text-base">Método de pago: {order.payment_method}</p>
                             <p className="text-xs lg:text-base">Fecha de compra: {order.order_date}</p>
                         </div>
+
                     </div>
                 )
             })}
